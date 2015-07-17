50236bc56ac81852381d4910646a0d0779e6775f
Nils Blum-Oeste <mail@nils-blum-oeste.net>
2015-07-17 21:21:11 +0200

    Add Prometheus instrumentation to service
    Actual target not yet setup in Prometheus config.

--- living-colors/project.clj
+++ living-colors/project.clj
@@ -1,6 +1,8 @@
 (defproject living-colors "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [ring/ring "1.4.0"]]
-  :ring {:handler living-colors.core/health-check}
+                 [ring/ring "1.4.0"]
+                 [com.soundcloud/prometheus-clj "1.0.6"]
+                 [compojure "1.4.0"]]
+  :ring {:handler living-colors.core/app}
   :main living-colors.core
   :aot [living-colors.core])
